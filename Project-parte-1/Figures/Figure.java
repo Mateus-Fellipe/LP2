621e211634a267a2dd086b45c253168cd1b9f1ed
mfruivo2 <51344364+mfruivo2@users.noreply.github.com>
2021-11-14 22:18:54 -0300

    Serialização, alteração no método drag e no paint

--- Project-parte-1/Figures/Figure.java
+++ Project-parte-1/Figures/Figure.java
@@ -2,17 +2,27 @@ package Figures;
 
 import java.awt.Graphics;
 import ivisible.IVisible;
+import java.io.Serializable;
 
 
-public abstract class Figure implements IVisible{
+public abstract class Figure implements IVisible, Serializable{
   public int x, y;
   public int w, h;
   protected int rC,gC,bC;
   protected int rF,gF,bF;
 
 
-
-  public abstract void paint(Graphics g);
-  public abstract void drag(int dir);
+  public abstract void paint(Graphics g, boolean selected);
   public abstract void newPos();
+  public void drag(int dir){
+    if (dir == 28){
+      this.x = this.x - 2;
+    } else if (dir == 29){
+      this.x = this.x + 2;
+    } else if (dir == 30){
+      this.y = this.y - 2;
+    } else{
+      this.y = this.y + 2;
+    }
+  }
 }
