9bac0f4a6bbf71727240e3c6344d9fec29a48c88
mfruivo2 <51344364+mfruivo2@users.noreply.github.com>
2021-11-14 22:15:26 -0300

    Alteração no método paint

--- Project-parte-1/ivisible/IVisible.java
+++ Project-parte-1/ivisible/IVisible.java
@@ -3,6 +3,6 @@ package ivisible;
 import java.awt.Graphics;
 
 public interface IVisible{
-  public void paint(Graphics g);
+  public void paint(Graphics g, boolean selected);
   public boolean clicked(int x, int y);
 }
