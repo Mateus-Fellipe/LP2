dcf6fc32a8cd280b7541463eea10a845cd10969d
mfruivo2 <51344364+mfruivo2@users.noreply.github.com>
2021-11-16 21:33:29 -0300

    Alteração dos modificadores de acesso
    Alteração dos modificadores públicos para protegidos e acréscimo dos métodos getter e setter.

--- Project-parte-1/Figures/Figure.java
+++ Project-parte-1/Figures/Figure.java
@@ -6,14 +6,15 @@ import java.io.Serializable;
 
 
 public abstract class Figure implements IVisible, Serializable{
-  public int x, y;
-  public int w, h;
+  protected int x, y;
+  protected int w, h;
   protected int rC,gC,bC;
   protected int rF,gF,bF;
 
 
   public abstract void paint(Graphics g, boolean selected);
   public abstract void newPos();
+  
   public void drag(int dir){
     if (dir == 28){
       this.x = this.x - 2;
@@ -21,8 +22,18 @@ public abstract class Figure implements IVisible, Serializable{
       this.x = this.x + 2;
     } else if (dir == 30){
       this.y = this.y - 2;
-    } else{
-      this.y = this.y + 2;
-    }
+    } else{ this.y = this.y + 2; }
   }
+  
+  public int getterX(){ return x; }
+  
+  public int getterY(){ return y; }
+
+  public void setterX(int aux){ this.x = aux; }
+
+  public void setterY(int aux){ this.y = aux; }
+  
+  public void setterW(int aux){ this.w = aux; }
+  
+  public void setterH(int aux){ this.h = aux; }
 }
